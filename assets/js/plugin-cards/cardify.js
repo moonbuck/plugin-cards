54afe214a279d6717848e60304c1c64cbf4ef512
Jason Cardwell <moonbuck@users.noreply.github.com>
2022-05-16 09:03:58 -0700

    Fixed loading of Twitter widgets

--- assets/js/plugin-cards/cardify.js
+++ assets/js/plugin-cards/cardify.js
@@ -1,5 +1,22 @@
 {{ with (.Scratch.Get "plugin-cards.Parameters").Cardify }}
 
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+  }(document, "script", "twitter-wjs"));
+
 /*
   Add the event listener for a loaded DOM. When invoked,
   the handler processes page links looking for the cardify
