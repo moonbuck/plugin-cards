d5646712d4274ed7f4bbeb443ec10bfad623b345
Jason Cardwell <moonbuck@users.noreply.github.com>
2022-05-16 09:22:08 -0700

    Hopefully accomplished what I claimed to have done last time.

--- assets/js/plugin-cards/cardify.js
+++ assets/js/plugin-cards/cardify.js
@@ -1,21 +1,26 @@
 {{ with (.Scratch.Get "plugin-cards.Parameters").Cardify }}
 
-window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
-
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
-
-  return t;
-  }(document, "script", "twitter-wjs"));
+/*
+  Load Twitter widgets waiting to be freed from HTML comment.
+*/
+if (typeof window.twttr === 'undefined') {
+  window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+  
+    return t;
+    }(document, "script", "twitter-wjs"));
+}
 
 /*
   Add the event listener for a loaded DOM. When invoked,
@@ -27,7 +32,6 @@ document.addEventListener('DOMContentLoaded',() => {
   // Fetch and process any eligible links with the query parameter.
   document.querySelectorAll('a.cardify').forEach(link => {
     link.outerHTML = link.childNodes[1].textContent;
-    twttr?.widgets.load();
     })
 });
 
